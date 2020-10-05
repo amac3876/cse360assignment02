86b3582b9fd648c964d4932abefd37918b150063
Anthony Mac <amac3@asu.edu>
2020-10-05 01:06:36 -0700

    Implemented all methods and updated documentation.

--- CSE360/src/cse360assignment02/AddingMachine.java
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -2,21 +2,25 @@ package cse360assignment02;
 
 /**
  * The AddingMachine program implements a class that can perform
- * addition and subtraction operations on a total.
+ * addition and subtraction operations on a total, and saves
+ * a record of all transactions in the history.
  * @author Anthony Mac
- * @version 1.0
+ * @version 2.0
  */
 public class AddingMachine {
   /**
  * The total variable stores the combined result of all transactions.
  */
 private int total;
-  
+ /**
+* The history variable records all transactions.
+*/
+private String history;
   /**
- * Constructor.
+ * Constructor. Initializes total as 0 and history as "0".
  */
 public AddingMachine () {
-    total = 0;  // not needed - included for clarity
+    clear();
   }
   
   /**
@@ -24,33 +28,40 @@ public AddingMachine () {
  * @return int
  */
 public int getTotal () {
-    return 0;
+    return total;
   }
   
   /**
- * This method adds the input to the total.
+ * This method adds the input to the total, and updates the history.
  * @param value This is the value added to the total.
  */
 public void add (int value) {
+	total += value;
+	history += " + " + value;
   }
 
   /**
- * This method subtracts the input from the total.
+ * This method subtracts the input from the total, and updates the history.
  * @param value This is the value subtracted from the total.
  */
 public void subtract (int value) {
+	total -= value;
+	history += " - " + value;
   }
 
   /**
- * This method returns a string representation of all transactions.
+ * This method returns a string representation of all transactions,
+ * which is the history.
  */
 public String toString () {
-    return "";
+    return history;
   }
 
   /**
  * This method clears the transactions and resets the total.
  */
 public void clear() {
+	total = 0;
+	history = "0";
   }
 }
